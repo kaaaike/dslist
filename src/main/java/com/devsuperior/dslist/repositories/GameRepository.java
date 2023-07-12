d74ff91014bdfbb711681ffa5ab9ce61cb7b4da1
Kaike Nascimento <kaike_soares123@hotmail.com>
2023-07-12 15:14:03 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
 
-// Consulta com o banco utilizando o framework JPA do Spring, agilidade em consultas no db.
+//Consulta com o banco utilizando o framework JPA do Spring, agilidade em consultas no db.
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 	
 }
+
+
