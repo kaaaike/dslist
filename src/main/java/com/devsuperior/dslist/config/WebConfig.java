6a36311ba7ccdb52db5acba8cbd3af6244153e72
Kaike Nascimento <kaike_soares123@hotmail.com>
2023-07-12 22:22:07 -0300

    Config CORS

---
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dslist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+	
+	
+	// Classe para identificar o CORS permitindo que ele seja responsável pelo endpoint de acesso
+	// Caso diferente do configurado em application.properties, receberá block em requests
+	@Configuration
+	public class WebConfig {
+
+		@Value("${cors.origins}")
+		private String corsOrigins;
+		
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+				}
+			};
+		}
+		
+	}
+
+}
